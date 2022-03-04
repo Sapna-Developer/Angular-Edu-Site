b63769aab5c42546724b88fb7d3b72af4a8b3977
Sapna-Developer <sapnapandey.sash@gmail.com>
2022-03-04 21:17:38 +0530

    10 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,11 +13,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialExampleModule} from './material.module';
 
 
-
-
 @NgModule({
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,59 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+
+export interface Course {
+  
+  IconUrl: string;
+  Name: string;
+  Version: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: [ './header.component.css' ]
 })
-export class HeaderComponent { }
+
+export class HeaderComponent { 
+
+  courseCtrl = new FormControl();
+  filteredCourses: Observable<Course[]>;
+
+  courses: Course[] = [
+    {
+      IconUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-3ebaf.appspot.com/o/Biology-01.jpeg?alt=media&token=8eea799d-29a3-4628-a384-e3a2d10e7c5a',
+      Version: 'EN-V1',
+      Name: 'Body Joints - Elbow'
+    },
+     {
+      IconUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-3ebaf.appspot.com/o/Biology-01.jpeg?alt=media&token=8eea799d-29a3-4628-a384-e3a2d10e7c5a',
+      Version: 'HI-V1',
+      Name: 'Body Joints - Ball-Socket Joint'
+    },
+     {
+      IconUrl: "https://firebasestorage.googleapis.com/v0/b/fir-3ebaf.appspot.com/o/Chemistry-01.jpeg?alt=media&token=92a52c20-0edb-43db-b006-4d5bf1724970",
+      Version: 'EN-V1',
+      Name: 'Chemical Change - Heating Sugar'
+    },
+     {
+      IconUrl: "https://firebasestorage.googleapis.com/v0/b/fir-3ebaf.appspot.com/o/Chemistry-01.jpeg?alt=media&token=92a52c20-0edb-43db-b006-4d5bf1724970",
+      Version: 'EN-V1',
+      Name: 'Acids-Bases - Conductivity',
+    },
+  ];
+  constructor() {
+    this.filteredCourses = this.courseCtrl.valueChanges.pipe(
+      startWith(''),
+      map(course => (course ? this._filteredCourses(course) : this.courses.slice())),
+    );
+  }
+
+  private _filteredCourses(value: string): Course[] {
+    const filterValue = value.toLowerCase();
+
+    return this.courses.filter(course => course.Name.toLowerCase().includes(filterValue));
+  }
+}
